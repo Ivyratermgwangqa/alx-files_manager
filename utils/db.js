ab8636568ddf7e675a14727cc72f0f604900d8d3
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-03 00:28:29 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,46 @@
 const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}`;
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+    
+    const url = `mongodb://${host}:${port}`;
+    
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = dbName;
+    this.connected = false;
+    this.db = null;
+  }
 
-        this.client = new MongoClient(url, { useUnifiedTopology: true });
-        this.client.connect().then(() => {
-            this.db = this.client.db(database);
-        }).catch((err) => console.error('MongoDB client error:', err));
+  async connect() {
+    if (!this.connected) {
+      await this.client.connect();
+      this.db = this.client.db(this.dbName);
+      this.connected = true;
     }
+  }
 
-    isAlive() {
-        return this.client.isConnected();
+  async isAlive() {
+    if (!this.connected) {
+      await this.connect();
     }
+    return this.connected;
+  }
 
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
-    }
+  async nbUsers() {
+    await this.connect();
+    return this.db.collection('users').countDocuments();
+  }
 
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
-    }
+  async nbFiles() {
+    await this.connect();
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
