4ff9ee4231f352da2d7a1e6f6c08b197e5f68491
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-03 00:20:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+const dbClient = require('./utils/db');
+const redisClient = require('./utils/redis');
+const UsersController = require('./controllers/UsersController');
+const FilesController = require('./controllers/FilesController');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(routes);
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// Define routes
+app.post('/users', UsersController.postNew);
+app.get('/users/me', UsersController.getMe);
+
+app.post('/files', FilesController.postUpload);
+app.get('/files/:id', FilesController.getShow);
+app.get('/files', FilesController.getIndex);
+app.put('/files/:id/publish', FilesController.putPublish);
+app.put('/files/:id/unpublish', FilesController.putUnpublish);
+app.get('/files/:id/data', FilesController.getFile);
+
+// Start the server
+app.listen(port, async () => {
+    await dbClient.connect();
+    await redisClient.connect();
+    console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
