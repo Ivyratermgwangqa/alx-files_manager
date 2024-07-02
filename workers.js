ea53a19ecb3a974aa14f1ecc11c14b9010c00447
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-02 04:39:29 +0200

    Update workers.js

---
+++ workers.js
@@ -0,0 +1,54 @@
+const Bull = require('bull');
+const imageThumbnail = require('image-thumbnail');
+const fs = require('fs').promises;
+const path = require('path');
+const dbClient = require('./utils/db');
+
+const fileQueue = new Bull('fileQueue');
+const userQueue = new Bull('userQueue');
+
+fileQueue.process(async (job, done) => {
+    const { fileId, userId } = job.data;
+
+    if (!fileId) {
+        return done(new Error('Missing fileId'));
+    }
+    if (!userId) {
+        return done(new Error('Missing userId'));
+    }
+
+    const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
+
+    if (!file) {
+        return done(new Error('File not found'));
+    }
+
+    const options = { width: [500, 250, 100] };
+
+    for (const width of options.width) {
+        const thumbnail = await imageThumbnail(file.localPath, { width });
+        const thumbnailPath = `${file.localPath}_${width}`;
+        await fs.writeFile(thumbnailPath, thumbnail);
+    }
+
+    done();
+});
+
+userQueue.process(async (job, done) => {
+    const { userId } = job.data;
+
+    if (!userId) {
+        return done(new Error('Missing userId'));
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+        return done(new Error('User not found'));
+    }
+
+    console.log(`Welcome ${user.email}!`);
+    done();
+});
+
+module.exports = { fileQueue, userQueue };
