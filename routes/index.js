1a64b69d68ee1d2f64063f5779e88807cd420045
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-04 03:30:50 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,34 @@
 const express = require('express');
-const FilesController = require('../controllers/FilesController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
 const router = express.Router();
+const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
+const AuthController = require('../controllers/AuthController');
 
+// User routes
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
 
+// File routes
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
+
+// Status routes
+router.get('/status', (req, res) => {
+    res.status(200).json({ redis: true, db: true });
+});
+
+router.get('/stats', async (req, res) => {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
+});
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 module.exports = router;
