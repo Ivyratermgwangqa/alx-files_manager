ec777131464fdb0d11c76e8457f7208bde452a9a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-07 02:12:35 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const dbClient = require('../utils/db');
 const UsersController = require('../controllers/UsersController');
 const FilesController = require('../controllers/FilesController');
 const AuthController = require('../controllers/AuthController');
